621615f81fce04b6190a7313fe16d8b50d876dba
Diego <g0d13@outlook.com>
2021-11-19 10:43:13 -0600

    create user module and add user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule, AccountsModule],
 })
 export class AppModule {}
